100476c595a908df4eef1b36ce41fbac6261ca24
bobbyv <vaughnwatson22@gmail.com>
2015-03-27 05:44:36 +0000

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 from flask.ext.marshmallow import Marshmallow
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://avogzdlajafwwd:pzWChTjmvTtZJ6W2PCQg8tSXsE@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d267het21etqvv'
 db = SQLAlchemy(app)
-
+app.config['SECRET_KEY'] = "works"
 
 from app import views, models
